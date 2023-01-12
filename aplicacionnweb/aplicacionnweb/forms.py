3c519469b44e0393b785b957ae4d511e42b317bd
goomeez21 <122268946+goomeez21@users.noreply.github.com>
2023-01-12 21:26:02 +0100

    Delete aplicacionnweb directory

--- aplicacionnweb/aplicacionnweb/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-class ContactForm(forms.Form):
-    email = forms.EmailField()
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(max_length=500)
